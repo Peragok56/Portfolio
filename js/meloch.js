f38da5b0a38d3991c5bebf9b502a0b29e41bb165
Peragok56 <72642924+Peragok56@users.noreply.github.com>
2022-11-25 21:44:55 +0500

    asdsda

--- js/meloch.js
+++ js/meloch.js
@@ -1,9 +1,12 @@
+// lock page
 function removeLock() {
     document.querySelector('body').classList.remove('lock')
 }
 
 setTimeout(removeLock, 5000);
 
+
+// Cards
 let cards = document.getElementsByClassName('card')
 
 for (let card of cards) {
@@ -14,4 +17,21 @@ for (let card of cards) {
             card.classList.add('activeTap')
         }
     })
+}
+
+
+// Scroll
+const anchors = document.querySelectorAll('a.scroll-to')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', (e) => {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href')
+    
+    document.querySelector(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
 }
